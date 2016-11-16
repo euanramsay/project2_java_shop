9925a5e263ab08eddc55f91f2c77a52291d08ab1
EuanRamsay <euancramsay@aol.com>
2016-11-16 10:03:27 +0000

    Removed Sale class

---
+++ javashop/Card.java
@@ -0,0 +1,12 @@
+package javashop;
+
+import java.util.Collections;
+
+public class Card {
+  private String name;
+  private int balance;
+
+public Card(String name, int balance){
+  this.name = name;
+  this.balance = balance;
+}
